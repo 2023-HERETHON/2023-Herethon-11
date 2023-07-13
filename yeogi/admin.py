705ea4dd860ea536a8546100c64c3d138a1235eb
s <80585231+soeun-nho@users.noreply.github.com>
2023-07-13 18:54:16 +0900

    Delete yeogi directory

--- yeogi/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Category, Review, Place, Street
-
-# Register your models here.
-admin.site.register(Category)
-admin.site.register(Review)
-admin.site.register(Place)
-admin.site.register(Street)
